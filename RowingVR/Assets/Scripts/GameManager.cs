402b2b21baf0f6de7af7ea44137839e12da8ca82
Lin Mao <marklinmao@gmail.com>
2018-04-04 13:50:31 +0300

    Create the terrain, water, boat, GameManager and PlayerControl scripts

---
+++ RowingVR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    //public GameObject canvasInit;
+    //public GameObject canvasPlaying;
+    //public GameObject canvasFailure;
+    //public GameObject canvasSuccess;
+    public const String sceneName = "Main";
+
+    public GameObject player;
+    //public GameObject timeSpentValue;
+
+    public enum GameState { InitState, PlayingState, FailureState, SuccessState };
+    public GameState currentState;
+
+
+    private PlayerController playerController;
+    private float lastStateChange = 0.0f;
+    private float maxLastingTimeAfterFailure = 5.0f;
+    private float successDistance = 5.0f;
+    private float timestampGameStart = 0.0f;
+    private float longpressTimeForReset = 3.5f;
+    private float timestampOfAppBtnDown = 0.0f;
+
+    private static float scoreTimeCost = 0f;
+    private static float scoreOxygenConsummed = 0f;
+    private static float highScore = 10000f;
+
+    void Start()
+    {
+        playerController = (PlayerController)player.GetComponent(typeof(PlayerController));
+        SetState(GameState.InitState);
+        timestampGameStart = Time.time;
+    }
+
+    void Update()
+    {
+
+        //Reset the scene
+        if (isAppBtnLongPressed(longpressTimeForReset))
+        {
+            SwitchToInitState();
+        }
+
+        //State switching for every state
+        switch (currentState)
+        {
+            case GameState.InitState:
+                enableSounds(false);
+                //canvasInit.SetActive(true);
+                //canvasPlaying.SetActive(false);
+                //canvasFailure.SetActive(false);
+                //canvasSuccess.SetActive(false);
+                break;
+
+            case GameState.PlayingState:
+                enableSounds(true);
+                //canvasInit.SetActive(false);
+                //canvasPlaying.SetActive(true);
+                //canvasFailure.SetActive(false);
+                //canvasSuccess.SetActive(false);
+
+                //if reaching the destination, switch to success state
+                //SwitchToSuccessState();
+
+                //if hitting the land, switch to failure state
+                //SwitchToFailureState();
+
+                break;
+
+            case GameState.FailureState:
+                enableSounds(false);
+                //canvasInit.SetActive(false);
+                //canvasPlaying.SetActive(false);
+                //canvasFailure.SetActive(true);
+                //canvasSuccess.SetActive(false);
+                if (GetStateElapsed() >= maxLastingTimeAfterFailure)
+                {
+                    SwitchToInitState();
+                }
+                break;
+
+            case GameState.SuccessState:
+                enableSounds(false);
+                //canvasInit.SetActive(false);
+                //canvasPlaying.SetActive(false);
+                //canvasFailure.SetActive(false);
+                //canvasSuccess.SetActive(true);
+
+                //calculate the time spent
+                scoreTimeCost = Time.time - timestampGameStart;
+
+                //Pause the world! This is a trick to pause the world without actually pausing everything...
+                Time.timeScale = 0.01f;
+                if (GetStateElapsed() > 0.05f)
+                {
+                    Time.timeScale = 1;
+                    SwitchToInitState();
+                }
+                break;
+        }
+        // Debug.Log ("Current state: " + currentState);
+    }
+
+    private void enableSounds(bool enabled)
+    {
+        //TODO all sounds to be enabled
+    }
+
+    private bool isAppBtnLongPressed(float lastingInSeconds)
+    {
+        if (GvrControllerInput.AppButtonDown)
+        {
+            timestampOfAppBtnDown = Time.time;
+        }
+        if (GvrControllerInput.AppButtonUp)
+        {
+            float timePassed = Time.time - timestampOfAppBtnDown;
+            return timePassed >= lastingInSeconds;
+        }
+        return false;
+    }
+
+    private void SetState(GameState state)
+    {
+        currentState = state;
+        lastStateChange = Time.time;
+    }
+
+    public void SwitchToPlayingState()
+    {
+        SetState(GameState.PlayingState);
+    }
+
+    public void SwitchToFailureState()
+    {
+        SetState(GameState.FailureState);
+    }
+
+    public void SwitchToSuccessState()
+    {
+        SetState(GameState.SuccessState);
+    }
+
+    public void SwitchToInitState()
+    {
+        SceneManager.LoadScene(GameManager.sceneName);
+        timestampGameStart = Time.time;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private float GetStateElapsed()
+    {
+        return Time.time - lastStateChange;
+    }
+
+    public bool IsInitState() { return currentState == GameState.InitState; }
+    public bool IsPlayingState() { return currentState == GameState.PlayingState; }
+    public bool IsFailureState() { return currentState == GameState.FailureState; }
+    public bool IsSuccessState() { return currentState == GameState.SuccessState; }
+
+}
---
+++ RowingVR/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,80 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine.UI;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private GameManager gameManager;
+    private Rigidbody rb;
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private Quaternion initialCameraRotation;
+
+    private float speed = 0f;
+    private Vector3 speedAccumulated = new Vector3(0, 0, 0);
+    public float speedNumber = 0f;
+    private float acceleration = 0.5f;
+
+    private float timestampOfClickBtnDown = 0.0f;
+    private float timeElapsed = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        initialCameraRotation = Camera.main.transform.rotation;
+        gameManager = (GameManager)Camera.main.GetComponent(typeof(GameManager));
+    }
+
+    void Update()
+    {
+        //show oxygen alarm screen
+        if (gameManager.IsPlayingState())
+        {
+
+        }
+
+        speedNumber = speedAccumulated.magnitude / 20;
+        //TODO: show speed value in canvas
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (gameManager.IsPlayingState())
+        {
+            // Speed up by clicking on TouchPad
+            if (GvrControllerInput.ClickButton)
+            {
+                speed += acceleration;
+
+                Vector3 targetPos = GvrControllerInput.Orientation * Vector3.forward;
+                Vector3 direction = targetPos;
+                rb.AddRelativeForce(direction.normalized * speed, ForceMode.Force);
+
+                speedAccumulated = direction.normalized * speed + speedAccumulated;
+            }
+            else
+            {
+                speed = 0;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Land"))
+        {
+            Debug.Log(">>>>>>>>>>>>>collision!>>>>>>>>>>>>>>>>>" + other.gameObject.tag + ", " + other.gameObject.name);
+            gameManager.SwitchToFailureState();
+        }
+    }
+
+}
